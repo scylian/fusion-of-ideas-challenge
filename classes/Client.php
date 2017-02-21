03e40c3e4871b167aebb2701b996a95edb2bbcaa
Bien Nguyen <bien.g.nguyen@gmail.com>
2017-02-21 12:22:38 -0800

    Update Client.php

--- classes/Client.php
+++ classes/Client.php
@@ -3,9 +3,6 @@
    * Client Class
    * This class is used to maintain client data and provide methods to
    * Add/Edit/Delete client data.
-   * 
-   * Additionally it will be used as the base class to be inherited
-   * by Sections and Links.
   **/
   
   include_once 'DB.php';
@@ -23,30 +20,6 @@
       }
       
       $this->db = $db;
-    }
-    
-    public function add() {
-      $this->child();
-      $this->data['name'] = $this->name;
-      $insert = $this->db->insert($this->tblName, $this->data);
-      $statusMsg = $insert ? 'Client data has been inserted successfully.' : 'Error occurred, try again.';
-      return $statusMsg;
-    }
-    
-    public function edit() {
-      $this->child();
-      $this->data['name'] = $this->name;
-      $this->condition['id'] = $this->id;
-      $update = $this->db->update($this->tblName, $this->data, $this->condition);
-      $statusMsg = $update ? 'Client data has been updated successfully.' : 'Error occurred, try again.';
-      return $statusMsg;
-    }
-    
-    public function delete() {
-      $this->condition['id'] = $this->id;
-      $delete = $this->db->delete($this->tblName, $this->condition);
-      $statusMsg = $delete ? 'Client data has been deleted successfully.' : 'Error occurred, try again.';
-      return $statusMsg;
     }
     
     public function child() {}
